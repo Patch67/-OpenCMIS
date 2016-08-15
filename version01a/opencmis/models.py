4037c94a764225905a471ebaccf42c9a9d971742
Patch67 <patrickabiggs@gmail.com>
2016-08-15 16:00:38 +0100

    Introduces BootStrap tabs. Started to create view to list Student Qualifications.

--- version01a/opencmis/models.py
+++ version01a/opencmis/models.py
@@ -4,17 +4,17 @@ from django.core.urlresolvers import reverse
 
 class Student(models.Model):
     gender_choices = (('M', 'Male'), ('F', 'Female'))
-    first_name = models.CharField(max_length=50, null=False)
-    last_name = models.CharField(max_length=30, null=False)
+    first_name = models.CharField(max_length=50, blank=False)
+    last_name = models.CharField(max_length=30, blank=False)
     gender = models.CharField(max_length=1, choices=gender_choices, default='M')
-    date_of_birth = models.DateField(null=True)
-    ULN = models.CharField(max_length=10, null=True)
-    house = models.CharField(max_length=50, null=True)
-    road = models.CharField(max_length=50, null=True)
-    area = models.CharField(max_length=50, null=True)
-    town = models.CharField(max_length=50, null=True)
-    post_code = models.CharField(max_length=12, null=True)
-    photo = models.FileField(blank=True, null=True)
+    date_of_birth = models.DateField(blank=False, null=True)
+    ULN = models.CharField(max_length=10, blank=True)
+    house = models.CharField(max_length=50, blank=True)
+    road = models.CharField(max_length=50, blank=True)
+    area = models.CharField(max_length=50, blank=True)
+    town = models.CharField(max_length=50, blank=True)
+    post_code = models.CharField(max_length=12, blank=True)
+    photo = models.FileField(blank=True)
 
     def __str__(self):
         return '%s %s' % (self.first_name, self.last_name)
@@ -43,8 +43,8 @@ class Qualification(models.Model):
 class StudentQualification(models.Model):
     student = models.ForeignKey('Student', on_delete=models.CASCADE)
     qualification = models.ForeignKey('Qualification', on_delete=models.CASCADE)
-    start = models.DateField(null=False)
-    expected_end = models.DateField(null=False)
+    start = models.DateField(null=False, blank=False)
+    expected_end = models.DateField(null=False, blank=False )
 
     def __str__(self):
         return '%s: %s' % (self.student, self.qualification)
