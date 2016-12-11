11a7f52c57b4fd2f6d5eb688113529268d593a22
Patch67 <patrickabiggs@gmail.com>
2016-12-11 10:26:53 +0000

    Changed to home function with tests for GET and POST

--- version01a/monitor/views.py
+++ version01a/monitor/views.py
@@ -1,10 +1,23 @@
-from django.views.generic import ListView
+from django.http import HttpResponse
+from django.shortcuts import render
 from .models import Device
 
 
-class Home(ListView):
-    model = Device
-    template_name = 'monitor/home.html'
-    queryset = Device.objects.all()
+def home(request):
+    """
+    This is an exploration of function based views.
+    In particular it is an exploration low level GET and POST methods
+    :param request:
+    :return:
+    """
+    context = {}
+    context['title'] = "Home"
+    if request.method == 'GET':
+        context['method'] = "GET"
+    elif request.method == 'POST':
+        context['method'] = "POST"
+    return render(request, 'monitor/home.html', context)
+
+
 
 
